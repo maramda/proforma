33a3e34b484e827defb5971b721e6ed945513782
maramda <maram.bendaoued2017@gmail.com>
2020-09-16 15:58:14 +0200

    commit

--- Model/Etudiant.class.php
+++ Model/Etudiant.class.php
@@ -29,7 +29,7 @@ $this->sexe = $sexe;
 
 public function ajouter(){ 
 
-include('../includes/connect.php');
+include('../includes/connect_db.php');
     
 	
 		$req = $bdd->exec ("INSERT INTO `etudiant`(
