6b96096920f911517f607d2620fbf8430fb39d05
maramda <maram.bendaoued2017@gmail.com>
2020-09-24 00:47:34 +0200

    commit

--- admin/includes/connect_db.php
+++ admin/includes/connect_db.php
@@ -1,10 +1,13 @@
 <?php
+$servername = "localhost";
+$login = "login";
+$password = "password";
 // Connexion à la base de données
 	try {
-		$bdd = new PDO ('mysql:host="localhost";dbname="proforma"', 'root', '');
+		$bdd = new PDO("mysql:host=$servername;dbname=proforma", 'root', '');
 
 	} catch(Exception $e) {
 		die('Erreur: '.$e->getMessage());
 		
 	}$bdd->query("SET NAMES UTF8")	
-	?>
+	?>è
